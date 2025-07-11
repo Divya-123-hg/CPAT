b39dcc3aaec1b9dee1accbc5cf02531015d2778b
Vaidya <mavaidy@tbdir.net>
2025-07-11 09:04:58 +0530

    Final Code

---
+++ app/admin/admin.component.spec.ts
@@ -0,0 +1,26 @@
+// To test whether the AdminComponent is created successfully.
+// This code is a unit test for an Angular component, written using Jasmine (a testing framework) and TestBed (Angular's testing utility).
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminComponent } from './admin.component';
+
+describe('AdminComponent', () => {
+  let component: AdminComponent;
+  let fixture: ComponentFixture<AdminComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AdminComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdminComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
